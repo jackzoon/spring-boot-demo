054486cd3597a8a7986bc4fd95be9b005428e6bd
Halen <911452497@qq.com>
2020-08-22 17:39:59 +0800

    spring-boot 集成 spring-boot-starter-actuator 用于监控 spring-boot 的启动和运行状态

---
+++ spring-boot-demo-actuator/src/main/java/com/halen/actuator/ActuatorDemo.java
@@ -0,0 +1,13 @@
+package com.halen.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorDemo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorDemo.class, args);
+    }
+
+}
