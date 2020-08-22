3eeca6dd154ef55d9b9b20f50d3c990483a7ed45
Halen <911452497@qq.com>
2020-08-22 17:17:46 +0800

    spring-boot 读取配置文件中的内容“

---
+++ spring-boot-demo-properties/src/main/java/com/halen/property/PropertyController.java
@@ -0,0 +1,23 @@
+package com.halen.property;
+
+import cn.hutool.core.lang.Dict;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PropertyController {
+    private final ApplicationProperty applicationProperty;
+    private final DeveloperProperty developerProperty;
+
+    @Autowired
+    public PropertyController(ApplicationProperty applicationProperty, DeveloperProperty developerProperty) {
+        this.applicationProperty = applicationProperty;
+        this.developerProperty = developerProperty;
+    }
+
+    @GetMapping("/property")
+    public Dict index() {
+        return Dict.create().set("applicationProperty", applicationProperty).set("developerProperty", developerProperty);
+    }
+}
