bb0b4228d0d60460f1f90b37987852143b9b5c36
Halen <911452497@qq.com>
2020-09-19 22:12:24 +0800

    freemarker

---
+++ spring-boot-demo-template-freemarker/src/main/java/com/halen/freemarker/SpringBootDemoTemplateFreemarkerApplication.java
@@ -0,0 +1,11 @@
+package com.halen.freemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoTemplateFreemarkerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoTemplateFreemarkerApplication.class, args);
+    }
+}
