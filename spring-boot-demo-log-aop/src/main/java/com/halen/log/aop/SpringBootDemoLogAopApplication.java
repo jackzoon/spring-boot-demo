8dc72168d70a35e690966a69ec3ad210ebdee855
Halen <911452497@qq.com>
2020-09-19 15:46:21 +0800

    spring-boot 使用AOP切面的方式记录web请求日志

---
+++ spring-boot-demo-log-aop/src/main/java/com/halen/log/aop/SpringBootDemoLogAopApplication.java
@@ -0,0 +1,11 @@
+package com.halen.log.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoLogAopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoLogAopApplication.class, args);
+    }
+}
