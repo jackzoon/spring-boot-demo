1971e6098ef16bf21b9b18e69b79e565afcc2422
Halen <911452497@qq.com>
2020-10-01 20:43:44 +0800

    beetl enjor template

---
+++ spring-boot-demo-template-beetl/src/main/java/com/halen/beetl/model/User.java
@@ -0,0 +1,9 @@
+package com.halen.beetl.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private String name;
+    private String password;
+}
---
+++ spring-boot-demo-template-enjoy/src/main/java/com/halen/enjoy/SpringBootDemoTemplateEnjoyApplication.java
@@ -0,0 +1,11 @@
+package com.halen.enjoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoTemplateEnjoyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoTemplateEnjoyApplication.class, args);
+    }
+}
