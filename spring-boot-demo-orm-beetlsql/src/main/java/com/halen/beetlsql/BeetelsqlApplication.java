23c366657af51a8c3df15d8c4502f67b7017cc63
张金弟 <jindi.zhang@renren-inc.com>
2020-10-04 21:53:33 +0800

    SpringBoot 整合beetlsql

---
+++ spring-boot-demo-orm-beetlsql/src/main/java/com/halen/beetlsql/BeetelsqlApplication.java
@@ -0,0 +1,11 @@
+package com.halen.beetlsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeetelsqlApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(BeetelsqlApplication.class, args);
+  }
+}
