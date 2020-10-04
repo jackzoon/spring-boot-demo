a1ffd6ccbf4bbeaab14a0463427e75a8f999ab64
张金弟 <jindi.zhang@renren-inc.com>
2020-10-04 21:22:20 +0800

    SpringBoot 集成Mybatis Plus

---
+++ spring-boot-demo-orm-mybatis-plus/src/main/java/com/halen/mybatis/mapper/RoleMapper.java
@@ -0,0 +1,7 @@
+package com.halen.mybatis.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.halen.mybatis.entity.Role;
+
+public interface RoleMapper extends BaseMapper<Role> {
+}
