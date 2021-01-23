e3e1b2517603f69a89506996301cc8af4f2e4a5c
Halen <911452497@qq.com>
2021-01-23 13:42:46 +0800

    email

---
+++ spring-boot-demo-email/src/test/java/com/halen/email/SpringBootDemoEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halen.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoEmailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
